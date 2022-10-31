00646deb16646a8c80a1d9ffcf7f601dd85ed63b
Yakov <yakov.zverev32@gmail.com>
2022-10-31 15:27:40 +0300

    first commit

---
+++ chat-frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Chat from './components/chat/chat';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import './App.scss';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={ <Chat />} />
+          <Route path='/login' element={ <Login />} />
+          <Route path='/register' element={ <Register />} />
+          <Route path="*" element={ <h1>404 Page not found.</h1> } />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
